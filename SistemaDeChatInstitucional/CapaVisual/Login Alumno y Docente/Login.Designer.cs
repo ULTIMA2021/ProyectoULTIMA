ccebc4aad7fd2ddebf219eed94729405883d9dd5
Andres Gomez <andres.gomez_72@hotmail.com>
2021-06-21 21:29:49 -0300

    Estructura de capas creada con el Login screen en la capa Visual

---
+++ SistemaDeChatInstitucional/CapaVisual/Login Alumno y Docente/Login.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace CapaVisual.Login_Alumno_y_Docente
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.btnExit = new System.Windows.Forms.Button();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.txtUusuario = new System.Windows.Forms.TextBox();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictVer = new System.Windows.Forms.PictureBox();
+            this.picOcultar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictVer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOcultar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(381, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(28, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(409, 399);
+            this.shapeContainer1.TabIndex = 1;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 54;
+            this.lineShape2.X2 = 353;
+            this.lineShape2.Y1 = 209;
+            this.lineShape2.Y2 = 209;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 55;
+            this.lineShape1.X2 = 354;
+            this.lineShape1.Y1 = 148;
+            this.lineShape1.Y2 = 148;
+            // 
+            // txtUusuario
+            // 
+            this.txtUusuario.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtUusuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUusuario.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUusuario.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.txtUusuario.Location = new System.Drawing.Point(55, 121);
+            this.txtUusuario.Multiline = true;
+            this.txtUusuario.Name = "txtUusuario";
+            this.txtUusuario.Size = new System.Drawing.Size(261, 20);
+            this.txtUusuario.TabIndex = 2;
+            this.txtUusuario.Text = "Usuario";
+            this.txtUusuario.Enter += new System.EventHandler(this.txtUusuario_Enter);
+            this.txtUusuario.Leave += new System.EventHandler(this.txtUusuario_Leave);
+            // 
+            // txtContra
+            // 
+            this.txtContra.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtContra.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContra.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContra.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.txtContra.Location = new System.Drawing.Point(55, 181);
+            this.txtContra.Multiline = true;
+            this.txtContra.Name = "txtContra";
+            this.txtContra.Size = new System.Drawing.Size(261, 20);
+            this.txtContra.TabIndex = 3;
+            this.txtContra.Text = "Contraseña";
+            this.txtContra.Enter += new System.EventHandler(this.txtContra_Enter);
+            this.txtContra.Leave += new System.EventHandler(this.txtContra_Leave);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLogin.Location = new System.Drawing.Point(158, 257);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(101, 34);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Si aun no te has registrado como usuario/a";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(288, 316);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(53, 13);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Click aqui";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(151, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictVer
+            // 
+            this.pictVer.Image = ((System.Drawing.Image)(resources.GetObject("pictVer.Image")));
+            this.pictVer.Location = new System.Drawing.Point(324, 172);
+            this.pictVer.Name = "pictVer";
+            this.pictVer.Size = new System.Drawing.Size(29, 29);
+            this.pictVer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictVer.TabIndex = 10;
+            this.pictVer.TabStop = false;
+            this.pictVer.Click += new System.EventHandler(this.picVer_Click);
+            // 
+            // picOcultar
+            // 
+            this.picOcultar.Image = ((System.Drawing.Image)(resources.GetObject("picOcultar.Image")));
+            this.picOcultar.Location = new System.Drawing.Point(324, 172);
+            this.picOcultar.Name = "picOcultar";
+            this.picOcultar.Size = new System.Drawing.Size(29, 29);
+            this.picOcultar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picOcultar.TabIndex = 11;
+            this.picOcultar.TabStop = false;
+            this.picOcultar.Click += new System.EventHandler(this.picOcultar_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(409, 399);
+            this.Controls.Add(this.pictVer);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtContra);
+            this.Controls.Add(this.txtUusuario);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.picOcultar);
+            this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictVer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOcultar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.TextBox txtUusuario;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictVer;
+        private System.Windows.Forms.PictureBox picOcultar;
+
+    }
+ }
