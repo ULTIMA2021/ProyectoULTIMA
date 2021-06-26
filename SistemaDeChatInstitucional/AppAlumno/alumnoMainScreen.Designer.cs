dc8817fc2e0b7beaf13da7318d01cbed4b374660
Andres Gomez <andres.gomez_72@hotmail.com>
2021-06-25 21:18:19 -0300

    Primera version de alumno main screen

--- SistemaDeChatInstitucional/AppAlumno/alumnoMainScreen.Designer.cs
+++ SistemaDeChatInstitucional/AppAlumno/alumnoMainScreen.Designer.cs
@@ -28,12 +28,301 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(alumnoMainScreen));
+            this.panelOpciones = new System.Windows.Forms.Panel();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.subMenuDocentes = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.subMenuMiPerfil = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnMiPerfil = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.fotoAlumno = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panelContenedor.SuspendLayout();
+            this.subMenuDocentes.SuspendLayout();
+            this.subMenuMiPerfil.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fotoAlumno)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelOpciones
+            // 
+            this.panelOpciones.BackColor = System.Drawing.Color.Blue;
+            this.panelOpciones.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelOpciones.Location = new System.Drawing.Point(0, 0);
+            this.panelOpciones.Name = "panelOpciones";
+            this.panelOpciones.Size = new System.Drawing.Size(855, 27);
+            this.panelOpciones.TabIndex = 0;
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.AutoScroll = true;
+            this.panelContenedor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.panelContenedor.Controls.Add(this.button6);
+            this.panelContenedor.Controls.Add(this.button5);
+            this.panelContenedor.Controls.Add(this.subMenuDocentes);
+            this.panelContenedor.Controls.Add(this.button1);
+            this.panelContenedor.Controls.Add(this.subMenuMiPerfil);
+            this.panelContenedor.Controls.Add(this.btnMiPerfil);
+            this.panelContenedor.Controls.Add(this.panel2);
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelContenedor.Location = new System.Drawing.Point(0, 27);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(191, 525);
+            this.panelContenedor.TabIndex = 1;
+            // 
+            // subMenuDocentes
+            // 
+            this.subMenuDocentes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(100)))));
+            this.subMenuDocentes.Controls.Add(this.button4);
+            this.subMenuDocentes.Controls.Add(this.button3);
+            this.subMenuDocentes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.subMenuDocentes.Location = new System.Drawing.Point(0, 337);
+            this.subMenuDocentes.Name = "subMenuDocentes";
+            this.subMenuDocentes.Size = new System.Drawing.Size(191, 91);
+            this.subMenuDocentes.TabIndex = 6;
+            this.subMenuDocentes.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(100)))));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(80)))), ((int)(((byte)(250)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(0, 48);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(28, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(191, 36);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Editar datos";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(100)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(80)))), ((int)(((byte)(250)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(0, 6);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(28, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(191, 36);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Editar datos";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(0, 301);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(14, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(191, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Docentes";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // subMenuMiPerfil
+            // 
+            this.subMenuMiPerfil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(100)))));
+            this.subMenuMiPerfil.Controls.Add(this.button2);
+            this.subMenuMiPerfil.Controls.Add(this.btnEditar);
+            this.subMenuMiPerfil.Dock = System.Windows.Forms.DockStyle.Top;
+            this.subMenuMiPerfil.Location = new System.Drawing.Point(0, 211);
+            this.subMenuMiPerfil.Name = "subMenuMiPerfil";
+            this.subMenuMiPerfil.Size = new System.Drawing.Size(191, 90);
+            this.subMenuMiPerfil.TabIndex = 4;
+            this.subMenuMiPerfil.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(100)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(80)))), ((int)(((byte)(250)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(0, 48);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(28, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(191, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Mis asignaturas";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(100)))));
+            this.btnEditar.FlatAppearance.BorderSize = 0;
+            this.btnEditar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(80)))), ((int)(((byte)(250)))));
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEditar.Location = new System.Drawing.Point(0, 6);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Padding = new System.Windows.Forms.Padding(28, 0, 0, 0);
+            this.btnEditar.Size = new System.Drawing.Size(191, 36);
+            this.btnEditar.TabIndex = 5;
+            this.btnEditar.Text = "Editar datos";
+            this.btnEditar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnMiPerfil
+            // 
+            this.btnMiPerfil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnMiPerfil.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMiPerfil.FlatAppearance.BorderSize = 0;
+            this.btnMiPerfil.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.btnMiPerfil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMiPerfil.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMiPerfil.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMiPerfil.Location = new System.Drawing.Point(0, 175);
+            this.btnMiPerfil.Name = "btnMiPerfil";
+            this.btnMiPerfil.Padding = new System.Windows.Forms.Padding(14, 0, 0, 0);
+            this.btnMiPerfil.Size = new System.Drawing.Size(191, 36);
+            this.btnMiPerfil.TabIndex = 2;
+            this.btnMiPerfil.Text = "Mi perfil";
+            this.btnMiPerfil.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMiPerfil.UseVisualStyleBackColor = false;
+            this.btnMiPerfil.Click += new System.EventHandler(this.btnMiPerfil_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblNombre);
+            this.panel2.Controls.Add(this.fotoAlumno);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(191, 175);
+            this.panel2.TabIndex = 3;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNombre.Location = new System.Drawing.Point(38, 127);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(94, 14);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre Apellido";
+            // 
+            // fotoAlumno
+            // 
+            this.fotoAlumno.Image = ((System.Drawing.Image)(resources.GetObject("fotoAlumno.Image")));
+            this.fotoAlumno.Location = new System.Drawing.Point(41, 6);
+            this.fotoAlumno.Name = "fotoAlumno";
+            this.fotoAlumno.Size = new System.Drawing.Size(117, 107);
+            this.fotoAlumno.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.fotoAlumno.TabIndex = 0;
+            this.fotoAlumno.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(191, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(664, 525);
+            this.panel1.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(0, 428);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(14, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(191, 36);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Agenda";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Location = new System.Drawing.Point(0, 464);
+            this.button6.Name = "button6";
+            this.button6.Padding = new System.Windows.Forms.Padding(14, 0, 0, 0);
+            this.button6.Size = new System.Drawing.Size(191, 36);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Mensajes";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // alumnoMainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(855, 552);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.panelOpciones);
+            this.Name = "alumnoMainScreen";
             this.Text = "alumnoMainScreen";
+            this.panelContenedor.ResumeLayout(false);
+            this.subMenuDocentes.ResumeLayout(false);
+            this.subMenuMiPerfil.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fotoAlumno)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelOpciones;
+        private System.Windows.Forms.Panel panelContenedor;
+        private System.Windows.Forms.Panel subMenuMiPerfil;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnMiPerfil;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.PictureBox fotoAlumno;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel subMenuDocentes;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
     }
 }
--- SistemaDeChatInstitucional/AppAlumno/alumnoMainScreen.cs
+++ SistemaDeChatInstitucional/AppAlumno/alumnoMainScreen.cs
@@ -16,5 +16,61 @@ namespace AppAlumno
         {
             InitializeComponent();
         }
+
+        public void esconderSubMenu()
+        {
+            if(subMenuMiPerfil.Visible == true)
+            {
+                subMenuMiPerfil.Visible = false;
+                
+                
+            }
+
+            if (subMenuDocentes.Visible == true)
+            {
+                subMenuDocentes.Visible = false;
+            }
+            
+        }
+
+        public void mostrarSubMenu(Panel subMenu)
+        {
+            if(subMenu.Visible == false)
+            {
+                esconderSubMenu();
+                subMenu.Visible = true;
+                
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+            
+        }
+
+        
+        private void btnMiPerfil_Click(object sender, EventArgs e)
+        {
+            mostrarSubMenu(subMenuMiPerfil);
+          //  this.btnMiPerfil.BackColor = Color.Blue;
+          //  this.button1.BackColor = Color.FromArgb(26, 32, 40);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mostrarSubMenu(subMenuDocentes);
+           // this.button1.BackColor = Color.Blue;
+           // this.btnMiPerfil.BackColor = Color.FromArgb(26, 32, 40);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            esconderSubMenu();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            esconderSubMenu();
+        }
     }
 }
