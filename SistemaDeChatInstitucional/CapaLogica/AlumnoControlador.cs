1af787cf77cc10103a0d395db93a426c49923f00
Federico Costa <fed.ger.co@gmail.com>
2021-07-15 15:15:46 -0300

    creacion de grupo,materia,orientacion

--- SistemaDeChatInstitucional/CapaLogica/AlumnoControlador.cs
+++ SistemaDeChatInstitucional/CapaLogica/AlumnoControlador.cs
@@ -60,45 +60,7 @@ namespace CapaLogica
             return lista(user, pass, p.validarAdmin);
         }
 
-        /*    public static bool isAlumno(string user, string pass)    ------------->>>> desde aca parte original de validar usuarios
-            {
-                Console.WriteLine("method isAlumno is running");
-                PersonaModelo p = new PersonaModelo();
-                List<PersonaModelo> personas = p.validarAlumno(user,pass);
-                if (personas.Count == 1)
-                {
-                    Session.saveToCache(personas[0]);
-                    return true;
-                }
-                return false;
-            } 
-
-
-         public static bool isDocente(string user, string pass)
-         {
-             Console.WriteLine("method isDocente is running");
-             PersonaModelo p = new PersonaModelo();
-             List<PersonaModelo> personas = p.validarDocente(user, pass);
-             if (personas.Count == 1)
-             {
-                 Session.saveToCache(personas[0]);
-                 return true;
-             }
-             return false;
-         }
-         public static bool isAdmin(string user, string pass)
-         {
-             Console.WriteLine("method isAdmin is running");
-             PersonaModelo p = new PersonaModelo();
-             List<PersonaModelo> personas = p.validarAdmin(user, pass);
-             if (personas.Count == 1)
-             {
-                 Session.saveToCache(personas[0]);
-                 return true;
-             }
-             return false;
-         } */            //    ----------------------->>>    hasta aca es la parte original de validar usuarios
-
+ 
         /*   public static DataTable listarMaterias()
            {
                PersonaModelo p = new PersonaModelo();
@@ -118,25 +80,6 @@ namespace CapaLogica
 
            */
 
-        /*   private static bool listaUsuarios(bool metodo)
-           {
-               PersonaModelo p = new PersonaModelo();
-               List<PersonaModelo> personas = p.metodo;
-               if (personas.Count == 1)
-               {
-                   Session.saveToCache(personas[0]);
-                   return true;
-               }
-               return false;
-
-
-           } */
-           
-            //retorna true si no existe el alumno en el sistema
-            //Andres esto lo arregle para que funcione, 
-            //pero creo que para verificar que una persona no este en el sistema se deberia hacer un metodo nuevo
-            //que busque en la tabla persona de una
-            //note que los metodos arriba no se pueden reusar para esto
         public static bool obtenerAlumno(string ci)
         {
             UsuarioModelo u = new UsuarioModelo();
@@ -144,7 +87,6 @@ namespace CapaLogica
                 return true;
             return false;
         }
-
 
         public static string obtenerDestinatario(string ci)
         {
@@ -157,7 +99,6 @@ namespace CapaLogica
             }
               return nombreDestinatario;
         }
-
 
         public static string obtenerRemitente(string ci)
         {
@@ -249,71 +190,6 @@ namespace CapaLogica
             return tabla;
         }
 
-        /*
-        public static DataTable ConsultasPrivadaEnviada()
-        {
-            ConsultaPrivadaModelo consulta = new ConsultaPrivadaModelo();
-            List<ConsultaPrivadaModelo> listaConsulta = null;
-            if (Session.type == 0)
-                listaConsulta = consulta.getConsultas(Session.cedula);
-            else if (Session.type == 1)
-                listaConsulta = consulta.getConsultas(Int32.Parse(Session.cedula));
-            DataTable tabla = new DataTable();
-            tabla.Columns.Add("idConsultaPrivada");
-            tabla.Columns.Add("idMensaje");
-            tabla.Columns.Add("ciDocente");
-            tabla.Columns.Add("ciAlumno");
-            tabla.Columns.Add("titulo de consulta");
-            tabla.Columns.Add("Status de consulta");
-            tabla.Columns.Add("Fecha Creada");
-            tabla.Columns.Add("Destinatario");
-            foreach (ConsultaPrivadaModelo c in listaConsulta)
-            {
-                tabla.Rows.Add(c.idConsultaPrivada, c.idMensaje, c.ciDocente, c.ciAlumno, c.titulo, c.cpStatus, c.cpFechaHora, c.ciDestinatario);
-            }
-            return tabla;
-        }
-        
-
-        public static DataTable ConsultasPrivada()
-        {
-            ConsultaPrivadaModelo consulta = new ConsultaPrivadaModelo();
-            List<ConsultaPrivadaModelo> listaConsulta = null;
-            if (Session.type == 0)
-                listaConsulta = consulta.getConsultasEnviadas(Session.cedula);
-            else if (Session.type == 1)
-                listaConsulta = consulta.getConsultasContestadas(Int32.Parse(Session.cedula));
-            DataTable tabla = new DataTable();
-            tabla.Columns.Add("idConsultaPrivada");
-            tabla.Columns.Add("idMensaje");
-            tabla.Columns.Add("ciDocente");
-            tabla.Columns.Add("ciAlumno");
-            tabla.Columns.Add("titulo de consulta");
-            tabla.Columns.Add("Status de consulta");
-            tabla.Columns.Add("Fecha Creada");
-            tabla.Columns.Add("Destinatario");
-            foreach (ConsultaPrivadaModelo c in listaConsulta)
-            {
-                tabla.Rows.Add(c.idConsultaPrivada, c.idMensaje, c.ciDocente, c.ciAlumno, c.titulo, c.cpStatus, c.cpFechaHora, c.ciDestinatario);
-            }
-            return tabla;
-        }   */
-        /*
-         method used to get the needed fields from the table the user is seeing
-         to run mensajesDeConsulta(int idConsultaPrivada, string ciAlumno,string ciDocente)
-
-          
-        public static List<string> getMessageThread(int selectedRowIndex){
-            List<string> dataToFindThread =new List<string>();
-            if (Session.type == 0)
-                listaConsulta = consulta.getConsultas(Session.cedula);
-            else if (Session.type == 1)
-                listaConsulta = consulta.getConsultas(Int32.Parse(Session.cedula));
-            DataTable tabla = new DataTable();
-            return g;
-        }
-        */
-
         public static void getMsgsFromConsulta(int idConsultaPrivada, string ciAlumno, string ciDocente) {
             MensajePrivadoModelo mpm = new MensajePrivadoModelo();
             foreach (MensajePrivadoModelo m in mpm.mensajesDeConsulta(idConsultaPrivada, ciAlumno, ciDocente))
@@ -334,10 +210,5 @@ namespace CapaLogica
             return mensajeEnviado;
         }
 
-        public static int getidCp_Mensaje(int idConsultaPrivada,string ci)
-        {
-
-            return 1;
-        }
     }
 }
