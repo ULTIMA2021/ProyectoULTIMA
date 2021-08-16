0d1b2858ba240a4152d9a4ab3fa5347c6d9f470d
Federico Costa <fed.ger.co@gmail.com>
2021-08-15 22:49:53 -0300

    fix chixo para replyscreen docente

--- SistemaDeChatInstitucional/AppDocente/menuScreens/replyScreen.Designer.cs
+++ SistemaDeChatInstitucional/AppDocente/menuScreens/replyScreen.Designer.cs
@@ -72,11 +72,14 @@
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(671, 347);
             this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.WrapContents = false;
             // 
             // replyScreen
             // 
