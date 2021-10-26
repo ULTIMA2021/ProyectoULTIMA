626fc7bcb3b0327be2d21386f24081b87b77c936
Ignacio Aguiar <nacho3562@hotmail.com>
2021-10-26 16:09:48 -0300

    test modelo, alta persona y sala, obtener persona y alumnoTemp

---
+++ SistemaDeChatInstitucional/PruebasUnitarias/TestSalaModelo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CapaDeDatos;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class TestSalaModelo
+    {
+        [TestMethod]
+        public void TestCrearSala()
+        {
+            try
+            {
+
+
+                SalaModelo sala = new SalaModelo();
+
+                    sala.idGrupo = int.Parse("1");
+                sala.idMateria = int.Parse("1");
+                sala.docenteCi = int.Parse("77777777");
+                sala.anfitrion = int.Parse("1");
+                sala.resumen = "resumen";
+                sala.creacion = sala.creacion;
+                sala.isDone = false;
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+        }
+    }
+}
