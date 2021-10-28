53d6ec9e1f473c4b2597498374ed5eda54f9d83a
Ignacio Aguiar <nacho3562@hotmail.com>
2021-10-28 16:44:57 -0300

    PrimeraVersion de pruebas unitarias

---
+++ SistemaDeChatInstitucional/PruebasUnitarias/TestGrupoModelo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CapaDeDatos;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class TestGrupoModelo
+    {
+        [TestMethod]
+        public void TestAltaGrupo()
+        {
+            try
+            {
+                GrupoModelo grupo = new GrupoModelo();
+                grupo.nombreGrupo = "Test";
+                grupo.crearGrupoNuevo();
+
+                Assert.IsTrue(true);
+            }
+            catch 
+            {
+
+                Assert.IsTrue(false);
+            }
+        }
+
+        [TestMethod]
+        public void TestAlumnoTieneGrupo(string ci)
+        {
+            try
+            {
+                GrupoModelo grupo = new GrupoModelo();
+                grupo.countAlumnoTieneGrupo("11111111");
+
+                Assert.IsTrue(true);
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+        }
+
+        [TestMethod]
+        public void TestDocenteTieneGrupo(string ci)
+        {
+            try
+            {
+                GrupoModelo grupo = new GrupoModelo();
+                grupo.countDocenteDictaGrupo("77777777");
+
+                Assert.IsTrue(true);
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+        }
+
+
+        [TestMethod]
+        public void TestIngresoAlumnoTieneGrupo(string alumnoCi, int idGrupo)
+        {
+            try
+            {
+                GrupoModelo grupo = new GrupoModelo();
+                grupo.nuevoIngresoAlumnoTieneGrupo("11111111", int.Parse("1"));
+
+                Assert.IsTrue(true);
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+        }
+
+
+        [TestMethod]
+        public void TestIngresoDocenteTieneGM(string docenteCi, int idGrupo, int idMateria)
+        {
+            try
+            {
+                GrupoModelo grupo = new GrupoModelo();
+                grupo.nuevoIngresoDocenteTieneGM("77777777", int.Parse("1"), int.Parse("1"));
+
+                Assert.IsTrue(true);
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+        }
+
+
+    }
+}
---
+++ SistemaDeChatInstitucional/PruebasUnitarias/TestMateriaModelo.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CapaDeDatos;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class TestMateriaModelo
+    {
+        [TestMethod]
+        public void TestAltaMateria()
+        {
+            try
+            {
+                MateriaModelo materia = new MateriaModelo();
+
+                materia.nombreMateria = "Biologia";
+                materia.crearMateriaNueva();
+                Assert.IsTrue(true);
+
+            }
+            catch 
+            {
+
+                Assert.IsTrue(false);
+            }
+            
+        }
+
+        [TestMethod]
+        public void TestEstadoMateria()
+        {
+            try
+            {
+                MateriaModelo materia = new MateriaModelo();
+
+                materia.actualizarEstadoDeMateria(true, "1");
+                Assert.IsTrue(true);
+                MateriaModelo mate = new MateriaModelo();
+                mate.actualizarEstadoDeMateria(false, "1");
+
+
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+
+            
+        }
+
+        [TestMethod]
+        public void TestNombreMateria()
+        {
+            try
+            {
+                MateriaModelo materia = new MateriaModelo();
+
+                materia.actualizarNombreDeMateria("test", "1");
+                Assert.IsTrue(true);
+
+                MateriaModelo mate = new MateriaModelo();
+                mate.actualizarNombreDeMateria("mat1", "1");
+
+
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+
+
+        }
+
+        [TestMethod]
+        public void TestGetMateria(string nombreMateria)
+        {
+            try
+            {
+                nombreMateria = "mat1";
+                MateriaModelo materia = new MateriaModelo();
+                materia.getMateria(nombreMateria);
+                Assert.IsNotNull(materia.idMateria);
+
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+        }
+
+        [TestMethod]
+        public void TestCountSalaMateria()
+        {
+            try
+            {
+
+                MateriaModelo materia = new MateriaModelo();
+                materia.idMateria = int.Parse("1");
+                materia.countSalaPorMateria();
+
+                Assert.IsTrue(true);
+
+            }
+            catch
+            {
+
+                Assert.IsTrue(false);
+            }
+        }
+
+    }
+}
